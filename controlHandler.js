c51670f9129f969235f9efb89d54821e5f85bc68
lonce wyse <cnmwll@nus.edu.sg>
2012-08-28 21:38:36 +0800

    initial commit for jsaPhoneControl

---
+++ controlHandler.js
@@ -0,0 +1,104 @@
+<!-- /* ---------------------------------------------------------------------------------------
+This jsaSound Code is distributed under LGPL 3
+Copyright (C) 2012 National University of Singapore
+Inquiries: director@anclab.org
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 3 of the License, or any later version.
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNULesser General Public License for more details.
+You should have received a copy of the GNU General Public License and GNU Lesser General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>
+------------------------------------------------------------------------------------------*/ -->// Kumar Subramanian (http://nishabdam.com) is the Guru responsible for the party node.js architecture! -->
+// Modified, adapted, and further messed with by:
+//	Lonce Wyse, July 2010
+//------------------------------
+// This is where we set up the responses to incoming control messages.
+controlHandler = function(){
+	
+	var m_sm;
+	var playingP=false;
+	
+	var myInterface = {}
+	myInterface.setSM = function(i_sm){
+		console.log("setting sound model in handler");
+		m_sm = i_sm;
+	}
+	
+    var init = function (){	
+	var foo;
+
+	// These are on the controlee web page:
+	var party_setup = document.getElementById('party_setup');
+	var party_box = document.getElementById('party_box');
+	var join_party_btn = document.getElementById('join_party');
+
+	join_party_btn.removeAttribute('disabled');
+
+	join_party_btn.onclick = function (e) {
+		var partyName = party_box.value.toLowerCase().replace(/[^a-z]/g, '');
+		party_box.value = "Joined!";
+		
+		console.log ("button click to join " + partyName);
+		
+		var socket = io.connect('animatedsoundworks.com:8000');
+		console.log ("joining using socket " + socket);
+		
+		
+		var handlers = {};
+
+		function defaultHandler(msg) {
+			console.error(JSON.stringify(msg));
+		}
+
+		function dispatch(msg) {
+			(handlers[msg.selector] || defaultHandler)(msg); // cool javascript pattern!!!! 
+		}
+
+		socket.on('connect', function () {
+			console.log ("socket.on ");
+			socket.on('message', function (msgStr) {
+				//console.log("-----");
+				//console.log ("dispatching message  " + msgStr);
+				dispatch(JSON.parse(msgStr));
+			});
+			console.log ("emit the register message  ");
+			socket.emit('register', { party: partyName, type: 'synth' });
+		});
+
+		handlers.morph = function (msg) {
+			
+			if(m_sm){
+				m_sm.setParamNorm(0, msg.x);
+				m_sm.setParamNorm(1, msg.y);
+			}				
+		};
+
+		handlers.play_stop = function (msg) {
+			if(m_sm && playingP){
+				m_sm.release();
+				//console.log("releaseing sound");
+				playingP=false;
+			} else{
+				m_sm.play();
+				//console.log("playing sound");
+				playingP=true;
+			}
+		};
+		
+		handlers.pitchroll = function (msg) {
+			if(m_sm){
+				m_sm.setParamNorm(2, msg.p);
+				m_sm.setParamNorm(3, msg.r);
+			} 
+		};
+
+		/*
+		setTimeout(function () { 
+			console.log("timeout");
+			party_setup.parentNode.removeChild(party_setup); 
+			party_setup = undefined;
+		}, 0);
+		*/
+		}
+	}();
+	
+	return myInterface;
+}
---
+++ message-router.js
@@ -0,0 +1,107 @@
+<!-- /* ---------------------------------------------------------------------------------------
+This jsaSound Code is distributed under LGPL 3
+Copyright (C) 2012 National University of Singapore
+Inquiries: director@anclab.org
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 3 of the License, or any later version.
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNULesser General Public License for more details.
+You should have received a copy of the GNU General Public License and GNU Lesser General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>
+------------------------------------------------------------------------------------------*/ -->/**
+ * A very simple soft realtime message router for the story telling
+ * sound rigs project. It uses the ultra simple socket.io library
+ * for the message passing and manages connections between two
+ * kinds of clients - "synth"s and "controller"s. One or more synths
+ * can be connected to one or more controllers and a group of synths
+ * and controllers form a "party", with each party given a name.
+ *
+ * To run this message router, you need Node.js and the socket.io
+ * library. 
+ *
+ * 1. Install Node.js (http://nodejs.org)
+ * 2. Install socket.io by running "npm install socket.io" from the
+ *    directory from which you'll be running the routing service.
+ *    (http://socket.io/)
+ * 3. Run the service using "node message-router.js". The service
+ *    will run on port 8000, so make sure that port is available.
+ * 
+ * Note that your synth and controller HTML+JS code needs to connect
+ * to the message router that you're running explicitly.
+ */
+
+var http = require('http');
+var fs = require('fs');
+
+var app = http.createServer(handler);
+var io = require('socket.io').listen(app);
+
+app.listen(8000);
+
+var parties = {}; 
+// A map from party name to a structure with info on
+// the synths and controllers that belong to the party.
+ 
+
+function handler(req, res) {
+    res.end();
+}
+
+function partyNamed(name) {
+    if (!name) { return undefined; }
+
+    if (name in parties) {
+        // Party already exists.
+        return parties[name];
+    } else {
+        // New party name.
+        return parties[name] = { name: name, synths: [], controllers: [] };
+    }
+}
+
+// Avoid verbose logging, which increases the response latency.
+io.set('log level', 1);
+
+// Forward messages between controllers and synths belonging to the
+// same party.
+io.sockets.on('connection', function (socket) {
+    // Upon first connection, synths and controllers send a 
+    // "register" message of the form {party: "party-name", type: "synth"}
+    // for synths and {party: "party-name", type: "controller"} for controllers.
+    // Watch for this message and join synths and controllers to their
+    // respective parties.
+    socket.on('register', function (data) {
+        var party = partyNamed(data.party);
+        if (party && data.type === 'synth') {
+            party.synths.push(socket);
+            socket.on('message', make_fwd(party.controllers));
+            socket.on('disconnect', make_on_disconnect('Synth', party, party.synths, socket));
+        } else if (party && data.type === 'control') {
+            party.controllers.push(socket);
+            socket.on('message', make_fwd(party.synths));
+            socket.on('disconnect', make_on_disconnect('Controller', party, party.controllers, socket));
+        }
+    });
+});
+
+function make_on_disconnect(type, party, things, socket) {
+    return function () {
+        var i, N;
+        for (i = 0, N = things.length; i < N; ++i) {
+            if (things[i] == socket) {
+                console.log(type + " " + (i+1) + " in party " + party.name + " disconnected.");
+                things.splice(i, 1); // Remove the thing that disconnected.
+                return;
+            }
+        }
+    };
+}
+
+function make_fwd(targets) {
+    return function (data) {
+        var i, N;
+        for (i = 0, N = targets.length; i < N; ++i) {
+            // Just pass the data on. The router doesn't
+            // care what the data turns out to be.
+            targets[i].send(data);
+        }
+    };
+}
