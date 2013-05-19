e629be579e12d7ee334f32cbff0c61ccdbb2f976
Pallav Shinghal <pshinghal@gmail.com>
2013-05-19 15:29:52 +0800

    Add authoring tool functionality.

--- scripts/author.js
+++ scripts/author.js
@@ -1,18 +1,41 @@
 define(
 	["controllerModel", "handlerModel", "Story"],
 	function (controllerModel, handlerModel, Story) {
+		var i;
+
+		// TODO IMPORTANT!
+		// THIS CODE SHOULD PROBABLY BE MOVED SOMEWHERE ELSE LATER!
+		var tempControllerModel = { interface: {} };
+		for (i = 0; i < controllerModel.interface.length; i++) {
+			var x;
+			var name = controllerModel.interface[i].paramioID;
+			tempControllerModel.interface[name] = {};
+			for (x in controllerModel.interface[i]) {
+				if (controllerModel.interface[i].hasOwnProperty(x) && x != "paramioID") {
+					tempControllerModel.interface[name][x] = controllerModel.interface[i][x];
+				}
+			}
+		}
+		controllerModel = tempControllerModel;
+		console.log(controllerModel);
+		// END IMPORTANT
+
 		var nextSceneId = 0;
 		var nextHandlerContentItemId = 0;
 		var nextModelId = 0;
 
+		// TODO: Re-populate and use in the code
 		var elemIds = {
 			scenesDiv: "scenes",
 			descriptionDiv: "description",
-			handlerContentsDiv: "handlerContents",
+			// handlerContentsDiv: "handlerContents",
+			sceneEditorDiv: "sceneEditor",
 			modelsDiv: "models",
 			newSceneButton: "newScene",
 			newHandlerContentItemButton: "newHandlerContentItem",
-			newModelButton: "newModel"
+			newModelButton: "newModel",
+			newSoundButton: "newSound",
+			newSoundNameField: "newSoundName"
 		};
 
 		var story = Story();
@@ -50,6 +73,11 @@ define(
 			for (x in elemIds) {
 				elements[x] = elem(elemIds[x]);
 			}
+		}
+
+		//TODO: Implement with checked sliderBox, etc.
+		function getCurrentSoundState(soundName) {
+			return null;
 		}
 
 		//===============
@@ -125,13 +153,14 @@ define(
 			console.log("Selecting");
 			console.log(sceneNode);
 			var currSceneId = story.getCurrentSceneId();
-			console.log("Current Adtive ID = " + currSceneId);
+			console.log("Current Active ID = " + currSceneId);
 			if (currSceneId > -1)
 				removeActive(elem("scene/" + currSceneId));
 			setActive(sceneNode);
 			var sceneId = atoi(sceneNode.getAttribute("id").split("/").pop());
 			console.log("This scene has ID=" + sceneId);
 			story.setCurrentScene(sceneId);
+			redrawSceneEditor();
 			console.log("Set Story's current scene to " + sceneId);
 		}
 
@@ -176,6 +205,7 @@ define(
 				removeSceneByButton(button);
 			};
 		}
+
 /*
 		function getNewHandlerContentItem() {
 			var hDiv = document.createElement("div");
@@ -228,7 +258,7 @@ define(
 			return function () {
 				removeHandlerContentItemById(id);
 			};
-		}*/
+		}
 
 		function getNewModel() {
 			var modelDiv = document.createElement("div");
@@ -243,7 +273,6 @@ define(
 			var deleteButton = document.createElement("button");
 			deleteButton.innerHTML = "Delete";
 			deleteButton.setAttribute("class", "deleteModel");
-			// TODO: Change this to the "ByButton" style
 			deleteButton.addEventListener("click", makeHandlerContentItemDeleter(nextModelId));
 			modelDiv.appendChild(deleteButton);
 
@@ -253,11 +282,283 @@ define(
 			nextModelId++;
 
 			return modelDiv;
+		}*/
+
+		function makeControllerName(name, type) {
+			var rowDiv = document.createElement("div");
+			rowDiv.setAttribute("class", "controllerNameRow");
+
+			var namePara = document.createElement("p");
+			namePara.setAttribute("class", "controllerName");
+			namePara.innerHTML = name;
+
+			rowDiv.appendChild(namePara);
+
+			return rowDiv;
+		}
+
+		function setStateByButton(button) {
+			// TODO: Add visual feedback (?)
+			var id = button.getAttribute("id");
+
+			var tokens = id.trim().split("/");
+			var address = tokens[0] + "/" + tokens[1];
+			//TODO: This depends on current soundName format. Generalise.
+			var soundName = tokens[2] + "/" + tokens[3];
+
+			story.getCurrentScene().setSoundState(address, soundName, getCurrentSoundState(soundName));
+		}
+
+		function makeStateSetter(button) {
+			return function (e) {
+				e.preventDefault();
+				setStateByButton(button);
+			};
+		}
+
+		function createSetStateButton(controllerName, paramName, soundName) {
+			var button = document.createElement("button");
+			button.setAttribute("class", "setStateButton");
+			button.setAttribute("id", controllerName + "/" + paramName + "/" + soundName);
+			button.innerHTML = "SET";
+
+			button.addEventListener("click", makeStateSetter(button));
+
+			return button;
+		}
+
+		function clearAllStateSetters() {
+			// TODO: IMPORTANT Handle crazy memory leaks! (I think)
+			var containers = document.getElementsByClassName("buttonContainer");
+			var i;
+			for (i = 0; i < containers.length; i++) {
+				while (containers[i].hasChildNodes())
+					containers[i].removeChild(containers[i].lastChild);
+			}
+		}
+
+		// Assumes sound has been added at the end
+		function populateStateSettersBySound(soundName) {
+			var containers = document.getElementsByClassName("buttonContainer");
+			var i;
+			for (i = 0; i < containers.length; i++) {
+				var tokens = containers[i].getAttribute("id").trim().split("/");
+				var button = createSetStateButton(tokens[0], tokens[1], soundName);
+				containers[i].appendChild(button);
+			}
+		}
+
+		function removeStateSettersBySound(soundName) {
+			var buttons = document.getElementsByClassName("setStateButton");
+			var i;
+			for (i = 0; i < buttons.length; i++) {
+				var button = buttons[i];
+				var id = button.getAttribute("id");
+				var tokens = id.trim().split("/");
+
+				//TODO: This depends on current soundName format. Generalise.
+				var currentSoundName = tokens[2] + "/" + tokens[3];
+
+				if (currentSoundName === soundName) {
+						button.parentNode.removeChild(button);
+				}
+			}
+		}
+
+		function populateStateSettersByContainer(buttonContainer, controllerName, paramName) {
+			var soundNames = story.getCurrentScene().getSoundNames();
+			var i;
+			for (i = 0; i < soundNames.length; i++) {
+				var button = createSetStateButton(controllerName, paramName, soundNames[i]);
+				buttonContainer.appendChild(button);
+			}
+		}
+
+		function makeControllerParamRow(controllerName, paramName, paramWords) {
+			var row = document.createElement("div");
+			row.setAttribute("class", "controllerParamRow");
+			row.setAttribute("id", controllerName + "/" + paramName);
+
+			var nameDiv = document.createElement("div");
+			nameDiv.setAttribute("class", "controllerParamRowName");
+
+			var namePar = document.createElement("p");
+			namePar.innerHTML = paramWords;
+			nameDiv.appendChild(namePar);
+			row.appendChild(nameDiv);
+
+			var buttonContainer = document.createElement("div");
+			buttonContainer.setAttribute("class", "buttonContainer");
+			buttonContainer.setAttribute("id", controllerName + "/" + paramName + "/buttonContainer");
+			populateStateSettersByContainer(buttonContainer, controllerName, paramName);
+
+			return row;
+		}
+
+		function makeRangeParamRows(name) {
+			var container = document.createElement("div");
+			container.setAttribute("class", "controllerParamContainer");
+
+			var minDiv = makeControllerParamRow(name, "min", "Min");
+			var maxDiv = makeControllerParamRow(name, "max", "Max");
+			container.appendChild(minDiv);
+			container.appendChild(maxDiv);
+
+			return container;
+		}
+
+		function makeNStateParamRows(name, num) {
+			var container = document.createElement("div");
+			container.setAttribute("class", "controllerParamContainer");
+
+			var i;
+			for (i = 0; i < num; i++) {
+				var stateRow = makeControllerParamRow(name, i, "State " + i); //Could make the "words" part 1-based instead
+				container.appendChild(stateRow);
+			}
+
+			return container;
+		}
+
+		function makeControllerParamRows(name, type, args) {
+			switch (type) {
+				case "range":
+					return makeRangeParamRows(name);
+				case "nState":
+					return makeNStateParamRows(name, args.numStates);
+				default:
+					console.log("Invalid type!");
+			}
+		}
+
+		function makeControllerBlock(name, type, args) {
+			var blockDiv = document.createElement("div");
+			blockDiv.setAttribute("class", "controllerBlock");
+			blockDiv.setAttribute("id", name);
+
+			var nameDiv = makeControllerName(name, type);
+			blockDiv.appendChild(nameDiv);
+
+			var paramRows = makeControllerParamRows(name, type, args);
+			blockDiv.appendChild(paramRows);
+
+			return blockDiv;
+		}
+
+		function addNewSound(soundModel) {
+			// TODO: Sanitize
+			// Note: Scene does NOT care about the models
+			var soundName = story.getCurrentScene().addSound(soundModel);
+			elem("soundList").appendChild(makeSoundBox(soundName));
+			populateStateSettersBySound(soundName);
+		}
+
+		function deleteSoundByButton(button) {
+			var tokens = button.getAttribute("id").trim().split();
+			var soundName = tokens[0] + "/" + tokens[1];
+			story.getCurrentScene().removeSoundByName(soundName);
+			var soundBox = elem(soundName + "/soundBox");
+			soundBox.parentNode.removeChild(soundBox);
+
+			removeStateSettersBySound(soundName);
+		}
+
+		function makeSoundDeleter(button) {
+			return function (e) {
+				e.preventDefault();
+				deleteSoundByButton(button);
+			};
+		}
+
+		function makeSoundBox(name) {
+			var soundDiv = document.createElement("div");
+			soundDiv.setAttribute("class", "sound");
+			soundDiv.setAttribute("id", name + "/soundBox");
+
+			var namePar = document.createElement("p");
+			namePar.innerHTML = name;
+			soundDiv.appendChild(namePar);
+
+			var deleteButton = document.createElement("button");
+			deleteButton.setAttribute("class", "soundDelete");
+			deleteButton.setAttribute("id", name + "/delete");
+			deleteButton.addEventListener("click", makeSoundDeleter(button));
+			soundDiv.appendChild(deleteButton);
+		}
+
+		function drawSoundList() {
+			var listDiv = elem("soundList");
+			var soundNames = story.getCurrentScene().getSoundNames();
+			var i;
+			for (i = 0; i < soundNames.length; i++) {
+				listDiv.appendChild(makeSoundBox(soundNames[i]));
+			}
+		}
+
+		function clearSoundList() {
+			var listDiv = elem("soundList");
+			while (listDiv.hasChildNodes()) {
+				listDiv.removeChild(listDiv.lastChild);
+			}
+		}
+
+		function redrawSoundList() {
+			clearSoundList();
+			drawSoundList();
+		}
+
+		function drawControllerEditors() {
+			var editorDiv = elem("controllerElementParams");
+			var x;
+			for (x in controllerModel) {
+				if (controllerModel.hasOwnProperty(x)) {
+					var name = x;
+					var type = controllerModel[x].eventType;
+					var args = {};
+					if (controllerModel[x].hasOwnProperty("numStates"))
+						args.numStates = controllerModel[x].numStates;
+
+					editorDiv.appendChild(makeControllerBlock(name, type, args));
+				}
+			}
+		}
+
+		function redrawStateSetters() {
+			// var controllerBox = elem("controllerElementParams");
+			// var soundsBox = elem("soundList");
+
+			// while (controllerBox.hasChildNodes())
+			// 	controllerBox.removeChild(controllerBox.lastChild);
+			clearAllStateSetters();
+
+			var soundNames = story.getCurrentScene().getSoundNames();
+
+			var i;
+			for (i = 0; i < soundNames.length; i++) {
+				populateStateSettersBySound(soundNames[i]);
+			}
+		}
+
+		function drawSceneEditor() {
+			drawSoundList();
+			drawControllerEditors();
+		}
+
+		function redrawSceneEditor() {
+			redrawSoundList();
+			redrawStateSetters();
+		}
+
+		function newSoundHandler() {
+			var model = elements.newSoundNameField.value;
+			addNewSound(model);
 		}
 
 		mapElementsToIds();
 
+		drawSceneEditor();
 		elements.newSceneButton.addEventListener("click", addNewScene);
 		elements.newHandlerContentItemButton.addEventListener("click", addNewHandlerContentItem);
+		elements.newSoundButton.addEventListener("click", newSoundHandler);
 	}
 );
