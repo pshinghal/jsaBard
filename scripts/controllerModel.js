f18cd9c2376128a85b62855c8fb9909808ff06b1
Pallav Shinghal <pshinghal@gmail.com>
2013-05-24 10:53:38 +0800

    Add new controller model.

--- scripts/controllerModel.js
+++ scripts/controllerModel.js
@@ -3,24 +3,77 @@ define(
 	function () {
 		//TODO: Add specific descriptions here. Append generic Handler descriptions TO that.
 		var myControllerModel = {
-			pitch: {
-				type:"range"
-			},
-			roll: {
-				type:"range"
-			},
-			toggle: {
-				type:"play_stop"
-			},
-			dummyTwoState: {
-				type:"nState",
-				arg: {
-					n: 2
+			"interface": [
+				{
+					"interfaceType": "prslider",
+					"eventType": "nState",
+					"paramioID": "prslider/36474",
+					"message": [
+						"mousedown"
+					],
+					"position": {
+						"x": "71.39%",
+						"y": "2.09%",
+						"width": "25.48%",
+						"height": "11.15%"
+					},
+					"orientation": "h",
+					"numStates": 2,
+					"color": "blue",
+					"invisible": ""
+				},
+				{
+					"interfaceType": "nStateButton",
+					"eventType": "nState",
+					"paramioID": "nStateButton/35027",
+					"message": [
+						"mousedown"
+					],
+					"position": {
+						"x": "9.62%",
+						"y": "16.55%",
+						"width": "81.73%",
+						"height": "24.56%"
+					},
+					"orientation": "h",
+					"numStates": 2,
+					"color": "green"
+				},
+				{
+					"interfaceType": "pushButton",
+					"eventType": "nState",
+					"paramioID": "pushButton/18312",
+					"message": [
+						"mousedown"
+					],
+					"position": {
+						"x": "10.58%",
+						"y": "47.91%",
+						"width": "80.05%",
+						"height": "26.66%"
+					},
+					"orientation": "h",
+					"numStates": 2,
+					"color": "#9900FF"
+				},
+				{
+					"interfaceType": "hslider",
+					"eventType": "nState",
+					"paramioID": "hslider/33536",
+					"message": [
+						"mousedown"
+					],
+					"position": {
+						"x": "9.62%",
+						"y": "85.71%",
+						"width": "81.73%",
+						"height": "7.49%"
+					},
+					"orientation": "h",
+					"numStates": 2,
+					"color": "#0099FF"
 				}
-			},
-			sceneChange: {
-				type:"scene_change"
-			}
+			]
 		};
 		return myControllerModel;
 	}
