a00c7abb8f09400123e6aef7adc9d4fe01127908
Pallav Shinghal <pshinghal@gmail.com>
2013-06-29 15:40:44 +0800

    Change party-control logic on player from 'input code' to 'create (and send) code'

--- scripts/sceneControlHandler.js
+++ scripts/sceneControlHandler.js
@@ -170,131 +170,145 @@ define(
 			}
 		};
 
+		function defaultHandler(msg) {
+			console.error(JSON.stringify(msg));
+		}
+
+		//TODO: (MAYBE) implement way of specifying whether we want to STOP or to RELEASE
+		// This currently interfaces with the sliderBox, so play and release are no different. i.e. play() while playing is the same as release()
+		// Keeping this at the moment, for possible future use.
+		var play_stop = function (targetModelName) {
+			console.log("Play/Stop " + targetModelName);
+			if (soundModels[targetModelName] && playingP[targetModelName]) {
+				// console.log("Yes, playing");
+				// console.log(soundModels[targetModelName]);
+				soundModels[targetModelName].release();
+				// console.log("releasing sound");
+				playingP[targetModelName] = false;
+			} else {
+				// console.log("Not playing");
+				// console.log(soundModels[targetModelName]);
+				soundModels[targetModelName].play();
+				// console.log("playing sound");
+				playingP[targetModelName] = true;
+			}
+		};
+
+		function dispatch(msg) {
+			var i, targetModelName, targetParamName, targetVal;
+			// (handlers[msg.selector] || defaultHandler)(msg); // cool javascript pattern!!!! 
+			var handlerName = msg.id;
+			var handler = m_scene.handlers[handlerName];
+
+			if (! handler){
+				//console.log("message " + handlerName + " not used in this scene");
+				return;
+			}
+
+			// TODO: Check that the parameter and the message have the same (or a compatible) TYPE
+			switch (handler.type) {
+			// TODO: Standardise the "val" part
+			case "range":
+				for (i = 0; i < handler.targets.length; i += 1) {
+					targetModelName = handler.targets[i].model;
+					targetParamName = handler.targets[i].parameter;
+					// console.log("got range message, targetModelName = " + targetModelName + ", and with soundModels = " + soundModels+ ", and soundModels[targetModelName] = " + soundModels[targetModelName]);
+					soundModels[targetModelName].setParamNorm(targetParamName, msg.val);
+				}
+				break;
+
+			//This is a special type.
+			//TODO: Have a more "standardised" version of this, too.
+			//Well, in a way, both are special types. Anything other than range and play/stop is unlikely to exist.
+			case "play_stop":
+				for (i = 0; i < handler.targets.length; i += 1) {
+					targetModelName = handler.targets[i].model;
+					play_stop(targetModelName);
+				}
+				break;
+
+			case "twoState":
+				console.log("Two state !!!");
+				currState[handlerName] = (currState[handlerName] + 1) % 2;
+				var targetState = currState[handlerName];
+				// console.log("Setting state " + targetState);
+				for (i = 0; i < handler.targets[targetState].length; i += 1) {
+					targetModelName = handler.targets[targetState][i].model;
+					targetParamName = handler.targets[targetState][i].parameter;
+					// If no value has been passed, the model will be updated with the dafault value
+					// IMPORTANT NOTE: This specifies an EXACT way in which the value must be passed to control a twoState type function.
+					// MAY NEED TO BE CHANGED
+					targetVal = (msg.val && msg.val[targetModelName] && msg.val[targetModelName][targetParamName]) ? msg.val : handler.targets[targetState][i].defaultValue;
+					soundModels[targetModelName].setParamNorm(targetParamName, targetVal);
+				}
+				break;
+
+			case "scene_change":
+				console.log("sceneChange!");
+				currentScene = (currentScene+1)%numScenes;
+				setScene(rig[currentScene]);
+				break;
+
+			default:
+				console.log("Bad parameter type!");
+			}
+		}
+
 		var initMessaging = function () {
-			// These are on the controlee web page:
-			var party_setup = document.getElementById('party_setup');
-			var party_box = document.getElementById('party_box');
-			var join_party_btn = document.getElementById('join_party');
+			var partyDiv = elem("partySelector");
+			partyDiv.removeAttribute("hidden");
+			var partyNameElem = elem("partyName");
 
-			party_box.removeAttribute('disabled');
-			join_party_btn.removeAttribute('disabled');
+			var makeParty = (function () {
+				var letters  = "abcdefghijklmnopqrstuvwxyz";
+				var category = "vcccvcccvcccccvcccccvccccc";
+				var consonants = "bcdfghjklmnpqrstvwxyz";
+				var vowels = "aeiou";
 
-			join_party_btn.onclick = function (e) {
-				var partyName = party_box.value.toLowerCase().replace(/[^a-z]/g, '');
+				return function (n) {
+					var result = [];
+					var i, k, a1, a2, a3;
+					for (i = 0; i < n; ++i) {
+						k = Math.floor(Math.random() * letters.length);
+						a1 = letters.charAt(k);
+						a2 = vowels.charAt(Math.floor(Math.random() * vowels.length));
+						a3 = consonants.charAt(Math.floor(Math.random() * consonants.length));
 
-				// Visual feedback
-				// TODO: Do this only when it's actually joined in the server
-				party_box.value = "Joined!";
-				join_party_btn.setAttribute('disabled');
-				party_box.setAttribute('disabled');
-
-				console.log("button click to join " + partyName);
-
-				var socket = io.connect(window.jsaHost);
-				console.log("joining using socket " + socket);
-
-
-				function defaultHandler(msg) {
-					console.error(JSON.stringify(msg));
-				}
-
-				//TODO: (MAYBE) implement way of specifying whether we want to STOP or to RELEASE
-				// This currently interfaces with the sliderBox, so play and release are no different. i.e. play() while playing is the same as release()
-				// Keeping this at the moment, for possible future use.
-				var play_stop = function (targetModelName) {
-					console.log("Play/Stop " + targetModelName);
-					if (soundModels[targetModelName] && playingP[targetModelName]) {
-						// console.log("Yes, playing");
-						// console.log(soundModels[targetModelName]);
-						soundModels[targetModelName].release();
-						// console.log("releasing sound");
-						playingP[targetModelName] = false;
-					} else {
-						// console.log("Not playing");
-						// console.log(soundModels[targetModelName]);
-						soundModels[targetModelName].play();
-						// console.log("playing sound");
-						playingP[targetModelName] = true;
+						if (category.charAt(k) === 'c') {
+							// First letter is a consonant. Make the second
+							// a vowel and the third a consonant.
+							result.push(a1 + a2 + a3);
+						} else {
+							// First letter is a vowel. Make the second a consonant
+							// and the third a vowel.
+							result.push(a1 + a3 + a2);
+						}
 					}
+					return result.join(' ');
 				};
+			}());
 
-				function dispatch(msg) {
-					var i, targetModelName, targetParamName, targetVal;
-					// (handlers[msg.selector] || defaultHandler)(msg); // cool javascript pattern!!!! 
-					var handlerName = msg.id;
-					var handler = m_scene.handlers[handlerName];
+			var partyName = makeParty(3);
 
-					if (! handler){
-						//console.log("message " + handlerName + " not used in this scene");
-						return;
+			var socket = io.connect(window.jsaHost);
+			socket.on("connect", function () {
+				console.log("Synth connected to server with party name: " + partyName);
+				var registerMessage = {
+					party: partyName.replace(/ /g, ''),
+					type: "synth"
+				};
+				socket.emit("register", registerMessage);
+				socket.on("confirm", function (data) {
+					if (data.party === registerMessage.party) {
+						partyNameElem.innerHTML = partyName;
+						socket.on("message", function (msgStr) {
+							dispatch(JSON.parse(msgStr));
+						});
+					} else {
+						partyNameElem.innerHTML = "Failed!";
 					}
-
-					// TODO: Check that the parameter and the message have the same (or a compatible) TYPE
-					switch (handler.type) {
-					// TODO: Standardise the "val" part
-					case "range":
-						for (i = 0; i < handler.targets.length; i += 1) {
-							targetModelName = handler.targets[i].model;
-							targetParamName = handler.targets[i].parameter;
-							// console.log("got range message, targetModelName = " + targetModelName + ", and with soundModels = " + soundModels+ ", and soundModels[targetModelName] = " + soundModels[targetModelName]);
-							soundModels[targetModelName].setParamNorm(targetParamName, msg.val);
-						}
-						break;
-
-					//This is a special type.
-					//TODO: Have a more "standardised" version of this, too.
-					//Well, in a way, both are special types. Anything other than range and play/stop is unlikely to exist.
-					case "play_stop":
-						for (i = 0; i < handler.targets.length; i += 1) {
-							targetModelName = handler.targets[i].model;
-							play_stop(targetModelName);
-						}
-						break;
-
-					case "twoState":
-						console.log("Two state !!!");
-						currState[handlerName] = (currState[handlerName] + 1) % 2;
-						var targetState = currState[handlerName];
-						// console.log("Setting state " + targetState);
-						for (i = 0; i < handler.targets[targetState].length; i += 1) {
-							targetModelName = handler.targets[targetState][i].model;
-							targetParamName = handler.targets[targetState][i].parameter;
-							// If no value has been passed, the model will be updated with the dafault value
-							// IMPORTANT NOTE: This specifies an EXACT way in which the value must be passed to control a twoState type function.
-							// MAY NEED TO BE CHANGED
-							targetVal = (msg.val && msg.val[targetModelName] && msg.val[targetModelName][targetParamName]) ? msg.val : handler.targets[targetState][i].defaultValue;
-							soundModels[targetModelName].setParamNorm(targetParamName, targetVal);
-						}
-						break;
-
-					case "scene_change":
-						console.log("sceneChange!");
-						currentScene = (currentScene+1)%numScenes;
-						setScene(rig[currentScene]);
-						break;
-
-					default:
-						console.log("Bad parameter type!");
-					}
-				}
-
-				socket.on('connect', function () {
-					console.log("socket.on ");
-					socket.on('message', function (msgStr) {
-						//console.log("-----");
-						//console.log ("dispatching message  " + msgStr);
-						dispatch(JSON.parse(msgStr));
-					});
-					console.log("emit the register message  ");
-					socket.emit('register', { party: partyName, type: 'synth' });
 				});
-
-				setTimeout(function () {
-					console.log("timeout");
-					//party_setup.parentNode.removeChild(party_setup);
-					//party_setup = undefined;
-				}, 0);
-			};
+			});
 		};
 
 		function loadStory() {
